0a800a18e0f19e1ea010ea43bdf3b17cea266468
Ahmed Ashour <a7med3shour10@gmail.com>
2025-09-30 21:31:01 +0300

    function added

--- oop.py
+++ oop.py
@@ -8,18 +8,25 @@ class BankAccount():
         self.amount=amount
         self.__balance += self.amount
         return  self.__balance
+    
     def withdraw(self,amount) :
         self.amount=amount
-        self.__balance -= self.amount
-        return  self.__balance
+        if self.amount <=self.__balance:
+            self.__balance -= self.amount
+            return  self.__balance
+        else:
+            return "your current balance doesnot enough"
+    
     def getbalance(self):
         return self.__balance
-    def setbalance (self, balance):
-        self.balance=balance
-        self.__balance=balance
-        return self.__balance
+    
+    # def setbalance (self, balance):
+    #     self.balance=balance
+    #     self.__balance=balance
+    #     return self.__balance
     
 c1=BankAccount(50000)
 print(c1.deposit(10000))
 print(c1.withdraw(5000))
-print(c1.setbalance(1000000))
+# print(c1.setbalance(1000000))
+print(c1.withdraw(100000000))
