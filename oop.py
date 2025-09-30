1eda8ccccf65e66125b839d31025e075f3c8e899
Ahmed Ashour <a7med3shour10@gmail.com>
2025-09-30 21:09:57 +0300

    initial commit

---
+++ oop.py
@@ -0,0 +1,25 @@
+class BankAccount():
+    def __init__(self,balance):
+        self.balance=balance
+        self.__balance=balance
+        print(f"Hello custmore your balance is {self.balance}")
+    
+    def deposit(self,amount):
+        self.amount=amount
+        self.__balance += self.amount
+        return  self.__balance
+    def withdraw(self,amount) :
+        self.amount=amount
+        self.__balance -= self.amount
+        return  self.__balance
+    def getbalance(self):
+        return self.__balance
+    def setbalance (self, balance):
+        self.balance=balance
+        self.__balance=balance
+        return self.__balance
+    
+c1=BankAccount(50000)
+print(c1.deposit(10000))
+print(c1.withdraw(5000))
+print(c1.setbalance(1000000))
